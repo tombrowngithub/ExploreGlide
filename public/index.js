38eaaed6ec3aeb621a7511daf9c72b965737d8e0
tombrowngithub <tombrownanuma@gmail.com>
2023-10-09 22:56:29 +0100

    Added orderBy, fixed contact us

--- public/index.js
+++ public/index.js
@@ -3,7 +3,9 @@ import {initializeApp} from "https://www.gstatic.com/firebasejs/10.4.0/firebase-
 import {
     getFirestore,
     collection,
-    getDocs,
+    orderBy,
+    query,
+    getDocs
 } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js"; // Import Firestore modules
 
 const firebaseConfig = {
@@ -27,62 +29,61 @@ function formatDate(date) {
         day: '2-digit' };
     return date.toLocaleDateString(undefined, options);
 }
-function renderPost(posts) {
-    const post = posts.data()
+function renderPost(postDoc) {
+    const post = postDoc.data();
     const postsContainer = document.getElementById("posts-container");
     const date = post.timestamp.toDate();
 
     // Format the date as a string
-    const Date =  formatDate(date); // Customize the format as needed
-    const time = date.toLocaleTimeString()
-    postsContainer.innerHTML = `
-          <div class="col-lg-6 col-xl-4">
-                <div class="blog-item wow fadeIn" data-wow-delay="0.1s">
-                    <div class="blog-img position-relative overflow-hidden">
-                        <img src="${post.imageUrl}" class="img-fluid w-100" alt="image-name">
-                        <div class="bg-primary d-inline px-3 py-2 text-center text-white position-absolute top-0 end-0">
-                            ${Date}
-                        </div>
-                    </div>
-                    <div class="p-4">
-                        <div class="blog-meta d-flex justify-content-between pb-2">
-                            <div class="">
-                                <small><i class="fas fa-user me-2 text-muted"></i>
-                                    <a href="" class="text-muted me-2">
-                                        By Admin
-                                    </a>
-                                </small>
-                            </div>
-                            <div class="">
-                               <small>
-                                   <i class="fa fa-clock text-muted me-1"></i><span>${time}</span>
-                                </small>
-                            </div>
-                        </div>
-                        <a href="" class="d-inline-block h5 mb-0">${post.title}</a>
-                        <p class="mb-4 article">${post.article.substring(0, 170)}...</p>
-                        <a href="/${posts.id}" class="btn btn-primary px-3">More Details</a>
-                    </div>
+    const formattedDate = formatDate(date); // Customize the format as needed
+    const time = date.toLocaleTimeString();
+
+    // Create a new post element
+    const postElement = document.createElement("div");
+    postElement.className = "col-lg-6 col-xl-4";
+    postElement.innerHTML = `
+        <div class="blog-item wow fadeIn" data-wow-delay="0.1s">
+            <div class="blog-img position-relative overflow-hidden">
+                <img src="${post.imageUrl}" class="img-fluid w-100" alt="image-name">
+                <div class="bg-primary d-inline px-3 py-2 text-center text-white position-absolute top-0 end-0">
+                    ${formattedDate}
                 </div>
             </div>
-    ${postsContainer.innerHTML}
-    `
+            <div class="p-4">
+                <div class="blog-meta d-flex justify-content-between pb-2">
+                    <div class="">
+                        <small><i class="fas fa-user me-2 text-muted"></i>
+                            <a href="" class="text-muted me-2">
+                                By Admin
+                            </a>
+                        </small>
+                    </div>
+                    <div class="">
+                        <small>
+                            <i class="fa fa-clock text-muted me-1"></i><span>${time}</span>
+                        </small>
+                    </div>
+                </div>
+                <a href="" class="d-inline-block h5 mb-0">${post.title}</a>
+                <p class="mb-4 article">${post.article.substring(0, 170)}...</p>
+                <a href="/${postDoc.id}" class="btn btn-primary px-3">More Details</a>
+            </div>
+        </div>
+    `;
 
+    // Append the post element to the posts container
+    postsContainer.appendChild(postElement);
 }
-
-
-
 
 // Function to fetch and render posts from Firestore
 async function fetchAndRenderPosts() {
     const postsCollection = collection(db, "posts"); // Change "posts" to your Firestore collection name
-    const querySnapshot = await getDocs(postsCollection);
+    const querySnapshot = await getDocs(query(postsCollection, orderBy("timestamp", "desc"))); // Use query with orderBy
 
     querySnapshot.forEach((doc) => {
         renderPost(doc);
     });
 }
-
 // Add an event listener to call fetchAndRenderPosts when the page loads
 window.addEventListener("load", () => {
     const explore = document.getElementById('exploreglide')
